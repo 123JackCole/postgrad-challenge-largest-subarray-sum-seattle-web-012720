86664b04c5a0f11c8196be0f042be25e1ef1c786
123JackCole <123jack.cole@gmail.com>
2020-11-18 21:18:48 -0800

    o^n solution

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4];
 
+// const largestSubarraySum = (arr) => {
+
+//     let total,
+//         max = 0;
+
+//     for (let i = 0; i < arr.length; i++) {
+//         total = 0;
+//         for (let j = i; j < arr.length; j++) {
+//             total += arr[j];
+//             if (total > max) max = total;
+//         }
+//     }
+
+//     return max;
+
+// }
+
 const largestSubarraySum = (arr) => {
 
-    let total,
-        max = 0;
+    [total, max] = [arr[0], arr[0]];
 
-    for (let i = 0; i < arr.length; i++) {
-        total = 0;
-        for (let j = i; j < arr.length; j++) {
-            total += arr[j];
-            if (total > max) max = total;
-        }
+    for (let i = 1; i < arr.length; i++) {
+        total = Math.max(arr[i], arr[i] + total);
+        max = Math.max(max, total);
     }
 
     return max;
 
 }
 
-console.log(largestSubArray(array));
+console.log(largestSubarraySum(array));

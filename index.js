0ed1a6564fe2fc9d52fa593d53149bf3d812dd17
123JackCole <123jack.cole@gmail.com>
2020-11-18 21:05:31 -0800

    completed naive approach

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-// write code here
+let array = [1, -1, 5, 3, -7, 4, 5, 6, -100, 4];
 
-// post work content
+const largestSubarraySum = (arr) => {
+
+    let total,
+        max = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        total = 0;
+        for (let j = i; j < arr.length; j++) {
+            total += arr[j];
+            if (total > max) max = total;
+        }
+    }
+
+    return max;
+
+}
+
+console.log(largestSubArray(array));
